8a41828f054d307f9e7f66a8955ce34db28e7742
alexmady <alexmady@gmail.com>
2016-09-06 23:00:13 +0100

    change some styling

--- app/app.ts
+++ app/app.ts
@@ -130,7 +130,7 @@ let config = {
   prodMode: false, 
   statusbarPadding: true, 
   // tabsHideOnSubPages: true,
-  backButtonText: '',
+  // backButtonText: '',
 };
 
 ionicBootstrap(MyApp, [FirebaseService], config); 
