46edf21c75b920673cc36f94933d27d0615c09cd
alexmady <alexmady@gmail.com>
2016-09-25 13:08:23 +0100

    make review stars bigger and change styling of completion

--- app/pages/completion/completion.ts
+++ app/pages/completion/completion.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Modal, NavParams, NavController, ViewController} from 'ionic-angular';
 import {FirebaseService} from '../../components/firebaseService';
+import {SecurityContext, DomSanitizationService} from '@angular/platform-browser';
 
 @Component({
   templateUrl: 'build/pages/completion/completion.html',
@@ -17,6 +18,7 @@ export class CompletionModal {
   constructor(private viewCtrl: ViewController,
     public navParams: NavParams,
     public nav: NavController,
+     private sanitizer: DomSanitizationService,
     public fbserv: FirebaseService) {
     console.log('nav params for completion modal', this.navParams);
     this.requestId = this.navParams.get('requestId');
@@ -25,12 +27,28 @@ export class CompletionModal {
     this.lastName = this.navParams.get('lastName');
   }
 
+  profileImage
+
+  getSafeURL(url) {
+    var safe_url = this.sanitizer.bypassSecurityTrustStyle('url(' + url + ')');
+    return safe_url;
+  }
+
   ngOnInit() {
 
     this.fbserv.getRequest(this.requestId)
       .subscribe((res: any) => {
         this.request = res.data;
         console.log('the request is: ', this.request);
+
+        let profile = this.request.quotes[this.supplierId].supplierProfile;
+
+        if (typeof profile.photo === 'string' && profile.photo.length > 0) {
+          var profileImage = profile.photo;
+          profileImage = profileImage.replace(/\r?\n|\r/g, '');
+          this.profileImage = this.getSafeURL(profileImage);
+        }
+
       });
   }
 
